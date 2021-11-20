6c0880abba36327a907f4e2da97d1ac95305552e
omarhazem2204 <omarhazem2204@gmail.com>
2021-11-20 20:43:49 +0100

    search setup

--- client/src/components/searchData.js
+++ client/src/components/searchData.js
@@ -1,9 +1,28 @@
-import React  from 'react';
-export default function searchData() {
-    var flight=JSON.stringify(JSON.parse(localStorage.getItem('flight')));
+import {React,useEffect,useState} from 'react';
+import axios from 'axios';
+
+export default function SearchData() {
+    var flight=JSON.parse(localStorage['flight']);
+    const[flightList, setFlightList]=useState([]);
+
+      useEffect(()=>{
+        axios.get('http://localhost:5000/flights').then((allFlights)=>{
+          setFlightList(allFlights.data);
+        }) 
+      },[])
+      var FlightList=JSON.stringify(flightList);
+      console.log(flightList);
+    //   var FlightList1=[{}];
+    //   for(var i=0;FlightList.length;i++){ 
+    //   }
+
     return(
         <div>
             {flight}
+            <br/>
+            <br/>
+            {FlightList}
+            )}
         </div>
     )
     }
