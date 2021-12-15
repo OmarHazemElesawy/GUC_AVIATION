f72bb9d1f0eb893268fd6b989eda6de175b69546
omarhazem2204 <omarhazem2204@gmail.com>
2021-12-15 21:41:32 +0100

    index changes

--- server/index.js
+++ server/index.js
@@ -2,6 +2,10 @@ import cors from 'cors';
 import mongoose from 'mongoose';
 import express from 'express';
 import flightRoutes from './routes/flight.js';
+import reservationRoutes from './routes/reservation.js';
+import userRoutes from './routes/user.js';
+import seatsRoutes from './routes/seats.js';
+
 const app=express();
 
 app.use(express.json({limit:"20mb", extended:true}));
@@ -10,6 +14,10 @@ app.use(express.urlencoded({limit:"20mb", extended:true}));
 app.use(cors());
 
 app.use('/flights',flightRoutes);
+app.use('/reservations',reservationRoutes);
+app.use('/users',userRoutes);
+app.use('/seats',seatsRoutes);
+
 
 const CONNECTION_URL=
 'mongodb+srv://vscode1357:vscode1357@cluster0.kxzfh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
